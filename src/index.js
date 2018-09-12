f43b36d40ef59d685cc1bcd6ba71ef0ff5037210
Adriano Falsarella Monte <adrifmonte@gmail.com>
2018-09-12 19:35:12 -0300

    Simplify arguments comparison

--- src/index.js
+++ src/index.js
@@ -7,15 +7,7 @@ function areArgumentsShallowlyEqual(equalityCheck, prev, next) {
     return false
   }
 
-  // Do this in a for loop (and not a `forEach` or an `every`) so we can determine equality as fast as possible.
-  const length = prev.length
-  for (let i = 0; i < length; i++) {
-    if (!equalityCheck(prev[i], next[i])) {
-      return false
-    }
-  }
-
-  return true
+  return !prev.some((prevValue, i) => !equalityCheck(prevValue, next[i]))
 }
 
 export function defaultMemoize(func, equalityCheck = defaultEqualityCheck) {
